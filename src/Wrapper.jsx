445eb0815d82b20e9df19e1df1284c8ec9aed8e2
Tynybek <tynybeko1122@gmail.com>
2023-03-17 14:45:11 +0600

    second

--- src/Wrapper.jsx
+++ src/Wrapper.jsx
@@ -3,6 +3,41 @@ import './App.scss';
 function Wrapper() {
   return (
     <div className="wrapper">
+      <div className="overlay">
+        <div className="drawer">
+          <h2>Корзина</h2>
+          <div className="items">
+            <div className="cartItem">
+              <div style={{ backgroundImage: 'url(/sneakers/1.jpg)' }} className="cartItemImg"></div>
+              <div>
+                <p>Мужские Кроссовки Nike Air Max 270</p>
+                <b>12 999 руб.</b>
+              </div>
+              <img className='remoteBtn' src="/img/btn_remote.svg" alt="Remote" />
+            </div>
+            <div className="cartItem">
+              <div style={{ backgroundImage: 'url(/sneakers/2.jpg)' }} className="cartItemImg"></div>
+              <div>
+                <p>Мужские Кроссовки Nike Air Max 270</p>
+                <b>12 999 руб.</b>
+              </div>
+              <img className='remoteBtn' src="/img/btn_remote.svg" alt="Remote" />
+            </div>
+          </div>
+          <ul className='drawer__footer'>
+            <li>
+              <span>Итого:</span>
+              <div></div>
+              <b>21 498 руб. </b>
+            </li>
+            <li>
+              <span>Налог 5%: </span>
+              <div></div>
+              <b>1074 руб. </b>
+            </li>
+          </ul>
+        </div>
+      </div>
       <header>
         <div className='headerLeft'>
           <img width={40} height={40} src='/img/logo.png' />
@@ -22,9 +57,18 @@ function Wrapper() {
         </ul>
       </header>
       <div className="content">
-        <h1>Все кроссовки</h1>
+        <div className='content__title'>
+          <h1>Все кроссовки</h1>
+          <div className="search-block">
+            <img src="/img/search.svg" alt="Search" />
+            <input type="text" placeholder='Search...' />
+          </div>
+        </div>
         <div className="sneakers">
           <div className="card">
+            <div className="favorite">
+              <img src="/img/heart__first.svg" alt="Unliked" />
+            </div>
             <img width={133} height={112} src="/sneakers/1.jpg" alt="sneakers" />
             <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
             <div>
